068db40e835e19be7e6460e46793465276f8c19a
LeLe962 <ysteamlqszl2@163.com>
2023-02-23 13:21:01 +0800

    same function but via py

--- blockinfo.py
+++ blockinfo.py
@@ -3,32 +3,57 @@ from web3.middleware import geth_poa_middleware
 # 连接到 Goerli 网络
 w3 = Web3(Web3.HTTPProvider('https://goerli.infura.io/v3/69b30b8f90e344ecbfe7509a436b8784'))
 w3.middleware_onion.inject(geth_poa_middleware,layer=0)
-# 获取最新区块号
-latest_block_number = w3.eth.blockNumber
 
-# 打开文件以写入内容
-with open('goerli_blocks.txt', 'w') as f:
-    # 循环遍历所有区块
-    for block_number in range(latest_block_number+1):
-        # 获取当前区块的信息
-        block = w3.eth.getBlock(block_number)
 
-        # 写入当前区块的信息到文件中
-        f.write(f'Block Number: {block.number}\n')
-        f.write(f'Timestamp: {block.timestamp}\n')
-        f.write(f'Hash: {block.hash.hex()}\n')
-        f.write(f'Parent Hash: {block.parentHash.hex()}\n')
-        f.write(f'Transactions: {block.transactions}\n')
-        f.write('\n')
+# 设置起始区块和结束区块
+start_block = 5539577
+end_block = 5539578
 
-        # 遍历当前区块的所有交易
-        for tx_hash in block.transactions:
-            # 获取当前交易的信息
-            tx = w3.eth.getTransaction(tx_hash)
+# 定义智能合约 ABI 获取函数
+def get_contract_abi(contract_address):
+    try:
+        contract_abi = w3.eth.contract(address=contract_address).abi
+    except InvalidAddress:
+        contract_abi = None
+    return contract_abi
 
-            # 写入当前交易的信息到文件中
-            f.write(f'Transaction Hash: {tx.hash.hex()}\n')
-            f.write(f'From: {tx["from"]}\n')
-            f.write(f'To: {tx.to}\n')
-            f.write(f'Value: {tx.value}\n')
-            f.write('\n')
+# 定义获取合约函数签名对应的 ABI 函数
+def get_function_abi(contract_address, function_signature):
+    contract_abi = get_contract_abi(contract_address)
+    if contract_abi is not None:
+        contract_instance = w3.eth.contract(address=contract_address, abi=contract_abi)
+        function_abi = next((x for x in contract_instance.abi if x['type'] == 'function' and function_signature in w3.sha3(text=x['name'] + '(' + ','.join([y['type'] for y in x['inputs']]) + ')').hex()), None)
+    else:
+        function_abi = None
+    return function_abi
+
+# 遍历指定区块并获取智能合约信息
+for block_number in range(start_block, end_block+1):
+    block = w3.eth.getBlock(block_number, True)
+    if block is not None and 'transactions' in block:
+        for tx in block['transactions']:
+            if tx['to'] is not None:
+                # 获取智能合约地址和函数签名
+                contract_address = tx['to']
+                function_signature = tx['input'][:10]
+                
+                # 获取智能合约 ABI 和函数 ABI
+                contract_abi = get_contract_abi(contract_address)
+                function_abi = get_function_abi(contract_address, function_signature)
+                
+                # 如果智能合约 ABI 和函数 ABI 都存在，则输出相应信息
+                if contract_abi is not None and function_abi is not None:
+                    print('Block:', block_number)
+                    print('Contract Address:', contract_address)
+                    print('Function Signature:', function_signature)
+                    print('Function Name:', function_abi['name'])
+                    print('Function Inputs:', function_abi['inputs'])
+                    print('Function Outputs:', function_abi['outputs'])
+                    print('Transaction Hash:', tx['hash'].hex())
+                    print('Transaction Index:', tx['transactionIndex'])
+                    print('From Address:', tx['from'])
+                    print('Gas Price:', tx['gasPrice'])
+                    print('Gas Used:', tx['gas'])
+                    print('Value:', tx['value'])
+                    print('---------------------------------------')
+                    
--- smartcontract_info.py
+++ smartcontract_info.py
@@ -1,1 +1,63 @@
 from web3 import Web3
+from web3.middleware import geth_poa_middleware
+from web3.exceptions import InvalidAddress
+import requests
+from lxml import etree
+# 连接到 Goerli 网络
+w3 = Web3(Web3.HTTPProvider('https://goerli.infura.io/v3/69b30b8f90e344ecbfe7509a436b8784'))
+w3.middleware_onion.inject(geth_poa_middleware,layer=0)
+# 获取指定区块的信息
+start_block = 5539575
+end_block = start_block+10
+
+# 定义智能合约 ABI 获取函数
+def get_contract_abi(contract_address):
+    try:
+        contract_abi = w3.eth.contract(address=contract_address).abi
+    except InvalidAddress:
+        contract_abi = None
+    return contract_abi
+
+# 定义获取合约函数签名对应的 ABI 函数
+def get_function_abi(contract_address, function_signature):
+    contract_abi = get_contract_abi(contract_address)
+    if contract_abi is not None:
+        contract_instance = w3.eth.contract(address=contract_address, abi=contract_abi)
+        function_abi = next((x for x in contract_instance.abi if x['type'] == 'function' and function_signature in w3.sha3(text=x['name'] + '(' + ','.join([y['type'] for y in x['inputs']]) + ')').hex()), None)
+    else:
+        function_abi = None
+    return function_abi
+
+# 将输出结果写入文件中
+with open('output.txt', 'w') as f:
+    # 遍历指定区块并获取智能合约信息
+    for block_number in range(start_block, end_block+1):
+        block = w3.eth.getBlock(block_number, True)
+        if block is not None and 'transactions' in block:
+            for tx in block['transactions']:
+                if tx['to'] is not None:
+                    # 获取智能合约地址和函数签名
+                    contract_address = tx['to']
+                    function_signature = tx['input'][:10]
+                    
+                    # 获取智能合约 ABI 和函数 ABI
+                    contract_abi = get_contract_abi(contract_address)
+                    function_abi = get_function_abi(contract_address, function_signature)
+                    
+                    # 如果智能合约 ABI 和函数 ABI 都存在，则输出相应信息
+                    if contract_abi is not None and function_abi is not None:
+                        output_str = f'Block: {block_number}\n' \
+                                     f'Contract Address: {contract_address}\n' \
+                                     f'Function Signature: {function_signature}\n' \
+                                     f'Function Name: {function_abi["name"]}\n' \
+                                     f'Function Inputs: {function_abi["inputs"]}\n' \
+                                     f'Function Outputs: {function_abi["outputs"]}\n' \
+                                     f'Transaction Hash: {tx["hash"].hex()}\n' \
+                                     f'Transaction Index: {tx["transactionIndex"]}\n' \
+                                     f'From Address: {tx["from"]}\n' \
+                                     f'Gas Price: {tx["gasPrice"]}\n' \
+                                     f'Gas Used: {tx["gas"]}\n' \
+                                     f'Value: {tx["value"]}\n' \
+                                     '---------------------------------------\n'
+                        f.write(output_str)
+                        print(output_str)
