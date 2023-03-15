f894b02222100597f11939ef055a7b09783d010a
Lele962 <ysteamlqszl2@163.com>
2023-03-15 21:39:20 +0800

    3.15-1

--- smartcontract_info.py
+++ smartcontract_info.py
@@ -6,7 +6,6 @@ addresses = []
 names = []
 count = 0
 
-
 def askURL():                                   # 获取当前页的智能合约地址
     head={                                      # 模拟浏览器头部信息
         "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
